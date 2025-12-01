ed741f3add1ab8b335a826121aa8e0537b361f68
Steven Trinh <strinh@gaming.tech>
2025-12-01 15:52:13 +0100

    line by point

--- Project SDL/main.cpp
+++ Project SDL/main.cpp
@@ -1,6 +1,29 @@
 #include <SDL.h>
 #include <iostream>
 
+void DrawHorizontalLine(SDL_Renderer* renderer, int x, int y, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderDrawPoint(renderer, (x + i), y);
+        SDL_RenderPresent(renderer);
+    }
+
+}
+
+void DrawVerticalLine(SDL_Renderer* renderer, int x, int y, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderDrawPoint(renderer, x, (y + i));
+        SDL_RenderPresent(renderer);
+    }
+
+}
+
+
 int main(int argc, char* argv[])
 {
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
@@ -18,7 +41,23 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    SDL_Delay(2000);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL, 0);
+    if (!renderer)
+    {
+        std::cout << "SDL_CreateRenderer Error : " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    /*SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderDrawPoint(renderer, 100, 100);
+    SDL_RenderPresent(renderer);*/
+
+    DrawHorizontalLine(renderer, 10, 10, 100);
+    DrawVerticalLine(renderer, 10, 10, 100);
+
+
+    SDL_Delay(4500);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
