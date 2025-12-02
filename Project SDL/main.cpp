6358adcbe294dd7ac80c6602854b2de5b642a9e6
Steven Trinh <strinh@gaming.tech>
2025-12-02 15:13:48 +0100

    go

--- Project SDL/main.cpp
+++ Project SDL/main.cpp
@@ -1,7 +1,6 @@
 #include <SDL.h>
 #include <iostream>
 
-float pi = 3.14159265358979f;
 //void DrawHorizontalLine(SDL_Renderer* renderer, int x, int y, int length)
 //{
 //    for (int i = 0; i < length; i++)
@@ -39,15 +38,38 @@ float pi = 3.14159265358979f;
 void DrawCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius, int precision)
 
 {
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    /*SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    float perimeter = 2 * M_PI;
+    float step = perimeter / precision; 
+
     for (int i = 0; i < precision; i++)
     {
-        SDL_RenderDrawPoint(renderer, ((centerX + radius) + i), (centerY + radius));
+        int x1 = radius * cos(step * i) + centerX;
+        int y1 = radius * sin(step * i) + centerY;
+        int x2 = radius * cos(step * (i + 1)) + centerX;
+        int y2 = radius * sin(step * (i + 1)) + centerY;
+
+        SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+    }
+    SDL_RenderPresent(renderer);*/
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    float perimeter = 2 * M_PI;
+    float step = perimeter / precision;
+
+    for (int i = 0; i < precision; i++)
+    {
+        int x1 = radius * cos(step * i) + centerX;
+        int y1 = radius * sin(step * i) + centerY;
+        int x2 = radius * cos(step * (i + 1)) + centerX;
+        int y2 = radius * sin(step * (i + 1)) + centerY;
+
+        SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
     }
     SDL_RenderPresent(renderer);
 }
 
-// etape 1 pi * precision 
+// etape 1 pi / precision 
 //etape 2 cos point etape 1 * i
 //etape pareil sin 
 // pareil cos sin i +1
